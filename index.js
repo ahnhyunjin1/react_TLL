23cc5d7f698a7c898b21c0831cf909d3ecc92596
ahnhyunjin1 <a01020813765@gmail.com>
2021-11-11 16:13:55 +0900

    react_Learning

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//applicateon/www-form 데이터 가져오기
+app.use(bodyParser.urlencoded({extended: true}));
+//json 타입 분석
+app.use(bodyParser.json());
+
 
 const mongoose = require("mongoose");
 const connect = mongoose.connect('mongodb+srv://anhyunjin:ahj961205@cluster0.qsqfx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
@@ -10,5 +18,23 @@ const connect = mongoose.connect('mongodb+srv://anhyunjin:ahj961205@cluster0.qsq
 
 
 app.get('/', (req, res) => res.send('Hello Woorld!'))
+
+app.post('/register', (req, res) => {
+    //회원가입 할때 필요한 정보들을 client에서 가져오기
+    // DB에 넣기
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+
+
+
 
 app.listen(port, () => console.log(`Examle app listening on port ${port}!`))
